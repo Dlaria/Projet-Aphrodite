70c87158831c7a147b30eaf62b5b7b32387a5197
Dlaria <airald.73@orange.fr>
2023-06-23 06:46:22 +0200

    maj carrousel

--- wp-content/themes/astra-child/index.php
+++ wp-content/themes/astra-child/index.php
@@ -6,20 +6,15 @@ get_header();
 
 if (have_posts()){
     echo '<h3 class="title-meddon">Chaussures grande taille pour femmes <br> à Paris depuis 1980</h3>';
-    // echo '<img src="wp-content/themes/astra-child/asset/Sacapuce/promotion-fs8.png" alt="Promotion">';
     echo '<div class="slide-container">
             <div class="custom-slider fade">
-                <div class="slide-index">1 / 3</div>
-                <img class="slide-img" src="wp-content/plugins/mon-plugin/asset/Sacapuce/promotion-fs8.png">
-                <div class="slide-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+                <img class="slide-img" src="wp-content/plugins/mon-plugin/asset/Sacapuce/promotion.png">
             </div>
             <div class="custom-slider fade">
-                <div class="slide-index">2 / 3</div>
-                <img class="slide-img" src="https://www.codeur.com/tuto/wp-content/uploads/2021/12/slide2.jpg">
-                <div class="slide-text">Nullam luctus aliquam ornare. </div>
+                <img class="slide-img" src="wp-content/plugins/mon-plugin/asset/Sacapuce/promotion2.png">
             </div>
-            <a class="prev" onclick="plusSlides(-1)">❮</a>
-            <a class="next" onclick="plusSlides(1)">❯</a>
+            <a class="prev" onclick="plusSlides(-1)"><img class="arrow-left" src="wp-content/plugins/mon-plugin/asset/Sacapuce/icons/ei_arrow-down.svg"></a>
+            <a class="next" onclick="plusSlides(1)"><img src="wp-content/plugins/mon-plugin/asset/Sacapuce/icons/ei_arrow-down.svg"></a>
         </div>
         <br>
         <div class="slide-dot">
