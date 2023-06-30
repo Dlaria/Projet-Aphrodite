046054ddd89ef7dd9c9d2ed392ffdb6bb5ff9819
Dlaria <airald.73@orange.fr>
2023-06-30 13:23:52 +0200

    Ajout de scripts

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -6,7 +6,8 @@ function enqueue_parent_styles() {
 
 
 function include_carrousel_js_file() {
-   wp_enqueue_script('carrousel_js', '../wp-content/themes/astra-child/includes/js/ac_script.js', array('jquery'), false, true);
+   wp_enqueue_script('carrousel_js', '../wp-content/themes/astra-child/includes/js/ac_carrousel_script.js', array('jquery'), false, true);
+   wp_enqueue_script('produit_js', '../wp-content/themes/astra-child/includes/js/ac_produit_script.js', array('jquery'), false, true);
 }
 add_action('wp_enqueue_scripts', 'include_carrousel_js_file');
 
